88a618f282680071e9f5ea088f5c59058ec24a95
Molly Brown <mollbrown@gmail.com>
2017-01-23 20:38:33 -0700

    add passing test for find single merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,7 @@ describe "Merchants API" do
   it "returns a list of merchants" do
     create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+    get '/api/v1/merchants.json'
 
     merchants = JSON.parse(response.body)
     merchant = merchants.first
@@ -23,4 +23,16 @@ describe "Merchants API" do
     expect(merchant["created_at"]).to be_a(String)
     expect(merchant["updated_at"]).to be_a(String)
   end
+
+  it "returns a single merchant by id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}.json"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+  end
+
 end
