6d0b8ff095350e575b487994327791f32a0e2c13
Molly Brown <mollbrown@gmail.com>
2017-01-23 23:10:55 -0700

    add passing spec tests for transactions index and show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124024520) do
+ActiveRecord::Schema.define(version: 20170124052958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20170124024520) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "invoice_id"
+    t.string   "credit_card_number"
+    t.string   "result"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id", using: :btree
   end
 
 end
---
+++ spec/factories/transactions.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :transaction do
+    credit_card_number "4242424242424242"
+    result "success"
+  end
+end
