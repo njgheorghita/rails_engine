6a0532bfac9233057bfa4b80f0ec4ec290843f20
Molly Brown <mollbrown@gmail.com>
2017-01-25 23:42:04 -0700

    add passing test for top merchants by items sold

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
+  def index
+    render json: Merchant.total_merchant_revenue_by_date(params[:date])
+  end
+
   def show
     render json: {"revenue" => Merchant.revenue(params[:id])}
   end
