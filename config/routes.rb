93966bc1a7b0295b2b7bcdecc3183050a251fde3
njgheorghita <njgheorghita@email.wm.edu>
2017-01-25 18:29:47 -0700

    top merchant biz intel endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
         get 'random',         to: 'random#show'
         get ':id/items',      to: 'items#index'
         get ':id/invoices',   to: 'invoices#index'
+        
       end
       resources :merchants, only: [:index, :show]
 
